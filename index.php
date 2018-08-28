b9a7544f3c64f37c167cbbcce7aaacc298f67d3c
Thando Mini <tzone85@gmail.com>
2018-08-28 17:17:15 +0200

    made use of the  super global

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-echo 'hello world';
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }            
+    </style>
+    
+</head>
+<body>
+    <header>
+        <h1><?php 
+
+            //making use of the super global
+            $name = $_GET['name'];
+            echo "Hello, $name"; 
+        
+        ?>
+        
+        </h1>
+
+    </header>
+</body>
+</html>
