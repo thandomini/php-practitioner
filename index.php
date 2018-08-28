2b7feaa21678ee8c6f1c896191ce5b18c76fdc96
Thando Mini <tzone85@gmail.com>
2018-08-28 18:10:11 +0200

    separated concerns and refered to the view of the file from the other index file

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<?php
+$greeting = 'hello world';
 
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }            
-    </style>
-    
-</head>
-<body>
-    <header>
-        <h1><?= "Hello, ".htmlspecialchars($_GET['name']); ?></h1>
-    </header>
-</body>
-</html>
+require 'index.view.php';
