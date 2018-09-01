f467c27ca49e2f74bf34ec17d02c1a6561d70eeb
Thando Mini <tzone85@gmail.com>
2018-09-01 03:24:05 +0200

    added classes. Revised arrays and switched access modifiers

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
 <?php
-require 'functions.php';
-$animals = [
-    'dog',
-    'cat'
+// require 'functions.php';
+
+class Task{
+    public $description;
+    public   $completed = false;
+
+    public function __construct($description){
+        // Automatically triggered on instantiation
+
+        $this->description = $description;
+    }
+
+    public function isComplete(){
+        return $this->completed;
+    }
+
+    public function complete(){
+        $this->completed = true;
+    }
+    
+}
+
+$tasks = [
+    new Task('Go to the store'),
+    new Task('Finish this tutorial'),
+    new Task('Prepare for the braai')
 ];
 
-$myAge = 18;
+$tasks[0]->complete();
 
-leagalAge($myAge);
+// dd($tasks);
 
-dd($animals);
-//require 'index.view.php';
+// $task->complete();
+//var_dump($task->isComplete());
+// var_dump($task->isComplete());
+// $task->complete();
+// dd($task);
+
+require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -4,36 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>practitioner</title>
+    <title>php practice</title>
 </head>
 <body>
-    <h1>Tasks for the day</h1>
+    
     <ul>
-        <li>
-            <strong>Name: </strong> <?= $tasks['title']; ?>
-        </li>
+        <?php foreach($tasks as $task) : ?>
 
-        <li>
-            <strong>Due Date: </strong> <?= $tasks['due']; ?>
-        </li>
+            <li>
 
-        <li>
-            <strong>Assigned To: </strong> <?= $tasks['assigned_to']; ?>
-        </li>
+                <?php if($task->completed): ?>
+                    <strike>
+                <?php endif; ?>
 
-        <li>
-            <!-- ternary operator isn't so good if we want to display some special html tags so reverting to the long way of doing this -->
-            <!-- <strong>Status: </strong> <?= $tasks['completed'] ? 'Finished' : 'Incomplete' ?> -->
-            
-            <strong>Status: </strong>
-            <?php  if ($tasks['completed']) : ?>
-                <span class="icon">&#9989</span>
-            <?php else: ?>
-                <span class="icon">&#x2715</span>
-            <?php endif; ?>
-        </li>
+                <?= $task->description; ?>
 
-        
+                <?php if($task->completed): ?>
+                    </strike>
+                <?php endif; ?>
+                
+            </li>
+        <?php endforeach; ?>
     </ul>
+    
 </body>
 </html>
